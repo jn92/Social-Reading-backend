989f127655424625c8174cbcca702eb4bf8df2ca
LAPTOP-574ELQ60\Jony <jmm92.jm@gmail.com>
2018-04-04 21:37:19 -0300

    Jony@LAPTOP-574ELQ60 MINGW64 /

---
+++ src/main/java/com/socialreading/socialreading/SocialreadingApplication.java
@@ -0,0 +1,12 @@
+package com.socialreading.socialreading;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocialreadingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SocialreadingApplication.class, args);
+	}
+}
